263718141f904698b29fa9344329c27ee4e2cbb7
Sarah Young <sarahyoung@Sarahs-MacBook-Pro.local>
2017-09-14 20:56:32 -0700

    tried some ec

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -4,14 +4,21 @@ pragma solidity ^0.4.13;
 contract greeter {
 	/* Add one variable to hold our greeting */
 	string greeting;
+	uint count;
 
 	function greeter(string _greeting) public {
 		/* Write one line of code for the contract to set our greeting */
 		greeting = _greeting;
+		count = 0;
 	}
 
 	function greet() constant returns (string)  {
 		/* Write one line of code to allow the contract to return our greeting */
+		count += 1;
 		return greeting;
+	}
+
+	function countGreets() constant returns (uint) {
+		return count;
 	}
 }
