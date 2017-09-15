00a99bb6a34d081539f48905d4e95f0178fe2faf
Sarah Young <sarahyoung@Sarahs-MacBook-Pro.local>
2017-09-14 20:47:22 -0700

    done

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,8 +2,18 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
+
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
+		
+		if(position <= 2) {
+			return 1;
+		} else {
+			return calculate(position - 1) + calculate(position - 2);
+		}
+
+
+
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
